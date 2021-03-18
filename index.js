88b960388292c1219d538b606995fe602c7667db
Luig1B <LuigiBenedetti2005@gmail.com>
2021-03-18 12:33:50 -0300

    more things

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("I'm not hiring if that's what you're looking for.");
+
+const codes = [
+	"#include <stdio.h>\nint main() {\n\tprintf(\"Hello, World!\\n\");\n}"
+];
+
+window.onload = function() {
+	let elements = document.getElementsByTagName("code");
+	for (let i = 0; i < elements.length; ++i) {
+		const el = elements.item(i);
+
+		el.innerText = codes[i];
+		el.style.tabSize = 4;
+	}
+}
